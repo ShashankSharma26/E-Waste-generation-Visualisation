ccf9d4fe0f38d1678e141b25f5310c42143b92ee
Shashank Sharma <ssha0028@gmail.com>
2019-07-02 16:54:42 +0530

    data edit

--- app.R
+++ app.R
@@ -1,3 +1,4 @@
+##to intall required packages
 #install.packages("leaflet")
 #install.packages("rgdal")
 #install.packages("plyr")
@@ -8,65 +9,37 @@
 #install.packages("leaflet.extras")
 #install.packages("tippy")
 #install.packages("bigrquery")
+#install.packages("shinyWidgets")
 
 
-#setwd("/Users/shashanksharma/Desktop/iter 1 final")
+
 ##libraries used
 library(leaflet)
-library(rgdal)
+# library(rgdal)
 library(plyr)
 library(reshape2)
 library(ggplot2)
-library(rmapshaper)
+# library(rmapshaper)
 library(leaflet.extras)
-library(tippy)
+# library(tippy)
 library(bigrquery)
+library(shinyWidgets)
 
-
-##data from the venar list fole named clean_data
-waste_data = read.csv("clean_data.csv",header = TRUE)
-#waste_data
-
-# Fix for Mornington Peninsula
-waste_data$Local.Government = revalue(waste_data$Local.Government, c("PENINSULA" = 'MORNINGTON PENINSULA'))
-
-##removing commas from numbers and then converting it ti numeric form
-waste_data$X2018 <- as.numeric(as.character(gsub(",","",waste_data$X2018)))
-waste_data$X2035 <- as.numeric(as.character(gsub(",","",waste_data$X2035)))
-
-
-
-##adding the 2018 and 2035 waste generated per LG
-waste_sum = setNames(aggregate(list(waste_data$X2018,waste_data$X2035), by=list(Category=waste_data$Local.Government), FUN=sum),c('Name',2018,2035))
-waste_sum$Name = trimws(waste_sum$Name)
-
-waste_sum = waste_sum[order(waste_sum$Name),]
-
-##counting collectin points per lg
-count_waste = setNames(as.data.frame(table(waste_data$Local.Government)),c('Name','Count'))
-#count_waste
-
-##loading shapefile
-spdf = readOGR(dsn = getwd(), layer = "VIC_LGA_POLYGON_shp")
-
-
-# Code that is used to make deployment version more lightweight.
-# spdf = rmapshaper::ms_simplify(spdf, keep = 0.05)
-# save.image("mapping_data.RData")
-# load("mapping_data.RData")
+##loading the mapping data saved using wrangling and integraton file to load the app quickly
+load("mapping_data.RData")
 
 
 ## Datafile for merging LGA data and contact information.
 ## Downloaded from Google Bigquery
 # Bigquery service token
-set_service_token('e-waste-database-2d9d97da8b37.json')
+#set_service_token('e-waste-database-2d9d97da8b37.json')
+# 
+# tb <- bq_dataset_query(
+#   x = "e-waste-database.db_council",
+#   query = "SELECT * FROM lga_details"
+# )
 
-tb <- bq_dataset_query(
-  x = "e-waste-database.db_council",
-  query = "SELECT * FROM lga_details"
-)
-
-citycode <- bq_table_download(tb)
+citycode <- read.csv("LOCAL_CODE.csv")
 
 citycode$Name = trimws(citycode$Name) # Trim ws
 names(citycode)[names(citycode) == "Code"] <- "LG_PLY_PID"
@@ -100,26 +73,26 @@ pal <- colorFactor(
   domain = spdf@data$`X2018`
 )
 
-######################################
-######USER INTERFACE CODE############
-#####################################
+##########################################
+######USER INTERFACE CODE#################
+##########################################
 
 ui <- fluidPage(tags$style(HTML("
-                                .tabs-above > .nav > li[class=active] > a {
-                                background-color: #000;
-                                color: #FFF;
-                                }")),
-  tabsetPanel(
+    .tabbable > .nav > li > a                  {background-color: #D3D3D3;  color:black}
+    .tabbable > .nav > li[class=active]    > a {background-color: grey; color:black}
+     ")),
+  setBackgroundColor("offwhite"),
+  tabsetPanel(                                             ##setting tab values
     tabPanel(
-      h6(strong("All Regions"),style = "color:blue;font-family: 'georgia';"), value = 1,##for all regions 
+      h6(strong("All Councils"),style = "color:black;font-family: 'arial';"), value = 1,##for all regions 
       
       
       mainPanel(
         conditionalPanel(
           condition = "(input.tabs == 1)",
-          h3("E-waste generation in Victoria", align = "center", style = "color:blue;font-family: 'georgia';"),
+          h3("E-waste generation in Victoria", align = "center", style = "color:black;font-family: 'arial';"),
           
-          leafletOutput("mymap",width = 1000, height = 600)
+          leafletOutput("mymap",width = 1200, height = 700)
         )
       )
     )
@@ -127,15 +100,15 @@ ui <- fluidPage(tags$style(HTML("
     ,
     
     tabPanel(
-      h6(strong("Compare Councils"),style = "color:blue;font-family: 'georgia';"), value = 2,##for comparison
+      h6(strong("Compare Councils"),style = "color:black;font-family: 'arial';"), value = 2,##for comparison
       sidebarLayout(
         sidebarPanel(
           selectInput("first",                                                 ##selection input
-                      h6(strong("Select first council"),style = "color:brown;font-family: 'georgia';"),
+                      h6(strong("Select first council"),style = "color:brown;font-family: 'arial';"),
                       choices = unique(boxplot_data$Name)
           ),
           selectInput("second",                                                 ##selection input
-                      h6(strong("Select second council"),style = "color:brown;font-family: 'georgia';"),
+                      h6(strong("Select second council"),style = "color:brown;font-family: 'arial';"),
                       choices = unique(boxplot_data$Name),
                       selected = c("ARARAT")
                       
@@ -152,11 +125,9 @@ ui <- fluidPage(tags$style(HTML("
 )
 
 
-
-######################################
-######SERVER CODE############
-#####################################
-
+##########################################
+#############SERVER CODE#################
+##########################################
 
 server <- function(input, output, session) {
   
@@ -164,9 +135,12 @@ server <- function(input, output, session) {
     ##leaflet visualising the data
     ##leaflet visualising the data
     leaflet(spdf) %>% 
-      #        addProviderTiles('CartoDB.PositronNoLabels') %>%
       addTiles() %>% 
-      setView(lat=-36.6, lng=145.5 , zoom=6.75) %>% 
+      
+      ###setting map view to victoria's lattitude and longitude#####
+      setView(lat=-36.6, lng=145.5 , zoom=7) %>% 
+      
+      ###adding poylgons for councils shapes####
       addPolygons(fillColor = ~pal(`X2018`),
                   fillOpacity = 0.5,
                   weight = 1,
@@ -174,30 +148,24 @@ server <- function(input, output, session) {
                                                color = "red",
                                                fillOpacity = 0.70,
                                                bringToFront = TRUE),
-                  popup= paste(strong("Council name:"),spdf@data$Name,"<br><br>",
+                  popup= paste(strong("Council name:"),spdf@data$Name,"<br><br>",                          ##for popup data
                                strong("E-waste generated in 2018:"),spdf@data$`X2018`," tonnes<br>",
-                               strong("E-waste estimated in 2035:"),spdf@data$`X2035`," tonnes<br><br>",
-                               strong("Council E-mail:"),spdf@data$Email,"<br>",
-                               strong("Council website:"),paste0( "<a href='http://", spdf@data$Website, "' target='_blank'>", "Click Here</a>")
-                               
-                  )
-                  ,
-                  label=~Name,
-                  group = spdf@data$Name)%>%
-      
-      
-      #addTooltip(session, id, title, placement = "bottom", trigger = "hover", options = NULL)
-      
-      
-      # addControl(
-      #   "<font><B>Search Councils below</B></font>",
-      #   position='topleft' ) %>%
-      
-      ##for searching purposes
-      addSearchFeatures( 
-        
+                               strong("E-waste estimated in 2035:"),spdf@data$`X2035`," tonnes<br><br>"
+                               ),
+                  label=~Name,                    ##for tooltip information
+                  group = spdf@data$Name
+                  )%>%
+     
+       ### to search for coouncils in the mapEEE
+      addSearchFeatures(                    
         targetGroups  = spdf@data$Name,
-        options = searchFeaturesOptions(position = "topleft",zoom=9.5, openPopup=TRUE,autoCollapse = FALSE, collapsed=FALSE, textPlaceholder = "Search councils")) %>%
+        options = searchFeaturesOptions(position = "topleft",
+                                        zoom=9.5, 
+                                        openPopup=TRUE,
+                                        autoCollapse = FALSE, 
+                                        collapsed=FALSE, 
+                                        textPlaceholder = "Search councils")
+                      ) %>%
       
         addLegend("topright", 
                 colors =c('darkred','orange','yellow','lightgreen','darkgreen'),
@@ -205,13 +173,10 @@ server <- function(input, output, session) {
                 title= "Tonnes of E-waste generated in 2018",
                 opacity = 1) %>%
 
-      # addControl(
-      #   "<P><center><B><font color='red'>Hint!</font></B></center>Search for council names<br/>Example:<br/><ul><li>Monash</li><li>Hume</li><li>Melbourne</li><li>Knox</li></ul></P>",
-      #   position='bottomright')%>%
       addControl(
         "<font><B>Reset Map Below</B></font>",
         position='topleft')%>%
-      addResetMapButton()
+      addResetMapButton()           ##to reset to default view
       
 
   })
@@ -225,12 +190,13 @@ server <- function(input, output, session) {
       plot_data = boxplot_data[which ((boxplot_data$Name == first_selection) | (boxplot_data$Name == second_selection)), ]
       
       ggplot(plot_data, aes(factor(plot_data$variable), plot_data$value, fill =plot_data$Name)) +
-        geom_bar(stat="identity", position = "dodge") +  ggtitle("Comparison of E-waste generated") +
-        scale_fill_brewer(palette = "Set1") +xlab("Year")+ylab("Tonnes of E-waste generated") + labs(fill = "Council") +
+        geom_bar(stat="identity", position = "dodge") +  
+        ggtitle("Comparison of E-waste generated") +
+        scale_fill_brewer(palette = "Set1") +xlab("Year")+
+        ylab("Tonnes of E-waste generated") + labs(fill = "Council") +
         theme(axis.text=element_text(size=18), axis.title=element_text(size=22), plot.title = element_text(size=28)
               ,legend.text = element_text(size=16), legend.title = element_text(size=22))
-      
-    }, height = 700, width = 700)
+      }, height = 700, width = 700)
   
 }
 
